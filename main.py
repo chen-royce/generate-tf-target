5ad5f789c656f312e1007c7a102864d513e96bad
Royce Chen <94400272+chen-royce@users.noreply.github.com>
2023-03-09 16:13:29 -0500

    feature: update msg format

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-BASE_COMMAND = 'terraform apply' # can swap this out for `terraform plan` as well
-
 def parse_file(filepath):
     ret = []
     file = open(filepath, 'r')
@@ -17,18 +15,18 @@ def omit_quotes(text):
         return text[1:len(text)-1]
 
 def generate_commands(parsed_file):
-    command = BASE_COMMAND
+    targets = ''
     num_targets, num_modules, num_resources = 0, 0, 0
     for line in parsed_file:
         if line[0] == 'module':
             module_name = omit_quotes(line[1])
-            command += ' -target=module.{0}'.format(module_name)
+            targets += ' -target=module.{0}'.format(module_name)
             num_targets += 1
             num_modules += 1
         if line[0] == 'resource':
             resource_type = omit_quotes(line[1])
             resource_name = omit_quotes(line[2])
-            command += ' -target={0}.{1}'.format(resource_type, resource_name)
+            targets += ' -target={0}.{1}'.format(resource_type, resource_name)
             num_targets += 1
             num_resources += 1
     print('---------------')
@@ -36,13 +34,15 @@ def generate_commands(parsed_file):
     print('---------------')
     print('- {0} module(s)'.format(num_modules))
     print('- {0} resource(s)\n'.format(num_resources))
-    return command
+    return targets
 
 # example command:
 # terraform apply -target="module.module_name" -target="resource_type.resource_name"
 def main():
     parsed_file = parse_file('./input.txt')
-    print('---------------\nCOMMAND TO RUN:\n---------------\n'+ generate_commands(parsed_file) + '\n')
+    targets = generate_commands(parsed_file)
+    print('---------------\nTERRAFORM PLAN:\n---------------\n'+ 'terraform plan' + targets + '\n')
+    print('---------------\nTERRAFORM APPLY:\n---------------\n'+ 'terraform apply' + targets + '\n')
 
 if __name__ == '__main__':
     main()
